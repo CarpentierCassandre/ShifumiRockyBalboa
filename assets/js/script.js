9014cd502372cf01b6e1f54b193c495c228a0a03
Carpentier Cassandre <carpentiercassandre@gmail.com>
2023-07-17 10:15:01 +0200

    ajout JS modale final et méthode pour réinitialiser le jeu

--- assets/js/script.js
+++ assets/js/script.js
@@ -11,8 +11,23 @@ let bigContainer= document.querySelector('.bigContainer');
 let soundBell = new Audio("assets/sound/clocheBox.mp3");
 let soundOfVictory= new Audio("assets/sound/victory.mp3");
 
-let soundOfTraining = new Audio("assets/sound/rocktrainingextrait.mp3.mp3");
+let soundOfTraining = new Audio("assets/sound/rocktrainingextrait.mp3");
 let soundOfDefeat = new Audio ("assets/sound/rockyDéfaite.mp3")
+
+let soundOfRocky_Adrian = new Audio ("assets/sound/rockybalboa/adrienne.mp3");
+let soundOfRocky_Annonce = new Audio ("assets/sound/rockybalboa/annonce.mp3");
+let soundOfRocky_AnnonceTwo = new Audio ("assets/sound/rockybalboa/annoncebis.mp3");
+let soundOfRocky_TakeYouBack= new Audio ("assets/sound/rockybalboa/takeyouback.mp3");
+
+let soundOfApollo_Annonce= new Audio ("assets/sound/apollocreed/annonce.mp3");
+let soundOfApollo_Medical= new Audio ("assets/sound/apollocreed/medical.mp3");
+let soundOfApollo_EyeOfTiger= new Audio ("assets/sound/apollocreed/eyeoftiger.mp3");
+let soundOfApollo_SoundTrack= new Audio ("assets/sound/apollocreed/soundtrack.mp3");
+
+let soundOfIvan_BreakYou= new Audio ("assets/sound/ivandrago/breakyou.mp3");
+let soundOfIvan_IfHeDies= new Audio("assets/sound/ivandrago/ifhedies.mp3");
+let soundOfIvan_Annonce = new Audio ("assets/sound/ivandrago/ivandragoannonce.mp3");
+let soundOfIvan_SoundTrack= new Audio ("assets/sound/ivandrago/soundtrackdrago.mp3");
 
 let answerUser = document.querySelector('#answerUser');
 let computerAnswer = document.querySelector('#computerAnswer');
@@ -32,18 +47,67 @@ let buttonCreed= document.querySelector('#buttonCreed');
 let boxBalboa=document.querySelector('#boxBalboa');
 let boxDrago=document.querySelector('#boxDrago');
 let boxCreed=document.querySelector('#boxCreed');
+let boxbis=document.querySelectorAll('.boxbis');
 
+//  refaire une partie 
+let buttontoDoAgain=document.querySelector('.toDoAgain');
+let buttonDontDoAgain=document.querySelector('.DontDoAgain');
+
+// function playSoundOfAdrian (){
+// soundOfRocky_Adrian.play();
+// }
+
+// function playSoundOfTakeYouBack(){
+//     soundOfRocky_TakeYouBack.play();
+// }
+
+// function playSoundOfAnnonce(){
+//     soundOfRocky_Annonce.play();
+// }
+
+// function playSoundOfAnnonceTwo(){
+//     soundOfRocky_AnnonceTwo.play();
+// }
+let soundOfHandleOne= new Audio ("assets/sound/coup depoing/1970.mp3");
+let soundOfHandleTwo= new Audio ("assets/sound/coup depoing/1976.mp3")
+let soundOfHandleThree= new Audio ("assets/sound/coup depoing/3454.mp3");
+
+
+// var randomIndex = Math.floor(Math.random() * userRandom[choix-1].sound.length)
+// var tableaubis= userRandom[choix-1].sound[randomIndex];
+// tableaubis.play();
+
+// test modal 
+let modal = document.querySelector("#myModal");
+
+let modalImage= document.querySelector(".modal-image");
+let modalbis=document.querySelector('.modal');
+let sentence=document.querySelector('.sentence');
+
+let numberRandomSound = Math.ceil(Math.random() * 4);
 let userRandom = [{
     name : 'Rocky Balboa',
-    img : 'rockyBalboa.png'
-}, {
+    img : 'rockyBalboa.png',
+    imgbis:'rockymvt.png',
+    sound:[soundOfRocky_Adrian,soundOfRocky_Annonce,soundOfRocky_AnnonceTwo,soundOfRocky_TakeYouBack]
+},
+ {
     name : 'Apollo Creed',
-    img : 'appoloCreed.png'
-}, {
+    img : 'appoloCreed.png',
+    imgbis:'creedmvtbis.png',
+    sound:[soundOfApollo_Annonce,soundOfApollo_EyeOfTiger,soundOfApollo_Medical,soundOfApollo_SoundTrack]
+}, 
+{
     name : 'Ivan Drago',
-    img : 'IvanDrago.png'
+    img : 'IvanDrago.png',
+    imgbis:'dragomvtbis.png',
+    sound:[soundOfIvan_Annonce,soundOfIvan_BreakYou,soundOfIvan_IfHeDies,soundOfIvan_SoundTrack]
 }];
 
+// for (s of userRandom[1].sound){
+// s.play();
+// }
+let soundOfHandle = [soundOfHandleOne,soundOfHandleTwo,soundOfHandleThree];
 function playSoundOfDefeat(){
     soundOfDefeat.play();
 }
@@ -57,57 +121,155 @@ function jouerSon() {
     soundBell.play();
 }
 
- buttonBalboa.addEventListener('mouseover',()=>{
-    boxBalboa.innerHTML='';
-    let newImageRocky = document.createElement("div");
-    newImageRocky.innerHTML=`<div><img src="assets/img/rockymvt.png"alt="image rocky "></div> ` ;
-    boxBalboa.appendChild(newImageRocky);
-    
- });
- buttonBalboa.addEventListener('mouseout',()=>{
-    boxBalboa.innerHTML='';
-    boxBalboa.innerHTML=`<div><img src="assets/img/rockyBalboa.png"alt="image rocky "></div> ` 
-
- })
- buttonDrago.addEventListener('mousemove',()=>{
-    boxDrago.innerHTML='';
-    let newImageDrago = document.createElement("div");
-    newImageDrago.innerHTML=`<div><img src="assets/img/dragomvtbis.png"alt="image Drago coup de poing"></div> ` ;
-    boxDrago.appendChild(newImageDrago);
-    
- });
-
- buttonCreed.addEventListener('mousemove',()=>{
-    boxCreed.innerHTML='';
-    let newImageCreed = document.createElement("div");
-    newImageCreed.innerHTML=`<div><img src="assets/img/creedmvtbis.png"alt="image Creed coup de poing"></div> ` ;
-    boxCreed.appendChild(newImageCreed);
- });
-
-
+// var isPlaying = false;
+// buttonBalboa.addEventListener('mouseover',()
 
 var choix = 1;
+var choicebis=1;
 for (let u of userChoice) {
+    // u.addEventListener('mouseover',()=>{ numberOfHandleSound.play();})
     u.addEventListener('click', () => {
+        // function handleClick() {
+        //     console.log("Clic !");
+        //   } 
         choix = u.getAttribute('choice');
-        answerUser.innerHTML = '';
-        //  answerUser.innerText = u.innerText;
-        let newImageBalboa2 = document.createElement('div');
-        newImageBalboa2.innerHTML = ` <img src="assets/img/${userRandom[choix - 1].img}" alt="${userRandom[choix - 1].name}"/><p>${userRandom[choix - 1].name}</p>`;
-        answerUser.appendChild(newImageBalboa2);
-    })
-}
+        
+         answerUser.innerHTML = '';
+         let newImageBalboa2 = document.createElement('div');
+         newImageBalboa2.innerHTML = ` <img src="assets/img/${userRandom[choix - 1].img}" alt="${userRandom[choix - 1].name}"/><p>${userRandom[choix - 1].name}</p>`;
+         answerUser.appendChild(newImageBalboa2);
+    
+           if(choix==1){
+              for  (us of userRandom[0].sound){
+                us.play();
+                us.currentTime=0;
+              };
+              for (us of userRandom[1].sound){
+                us.pause();
+                us.currentTime=0;
+              }
+              for (us of userRandom[2].sound)us.pause(); 
+              
+           } else if (choix==2){
+            for  (us of userRandom[0].sound){
+                us.pause();
+                us.currentTime=0;
+              };
+              for (us of userRandom[1].sound){
+                us.play();
+                us.currentTime=0;
+              }
+              for (us of userRandom[2].sound)
+              us.pause(); 
+              us.currentTime=0;
 
+            }else if (choix==3){
+            for  (us of userRandom[0].sound){
+                us.pause();
+                us.currentTime=0;
+              };
+              for (us of userRandom[1].sound){
+                us.pause();
+                us.currentTime=0;
+              }
+              for (us of userRandom[2].sound)
+              us.play();us.currentTime=0; 
+          }
+ 
+  var randomIndex = Math.floor(Math.random() * userRandom[choix-1].sound.length)
+//   var tableaubis= userRandom[choix-1].sound[randomIndex];
+
+// userRandom[choix-1].sound[1].play()
+if (randomIndex ==0){
+ userRandom[choix-1].sound[0].play();
+ userRandom[choix-1].sound[0].currentTime=0;
+ userRandom[choix-1].sound[1].pause();
+ userRandom[choix-1].sound[1].currentTime=0;
+ userRandom[choix-1].sound[2].pause();
+ userRandom[choix-1].sound[2].currentTime=0;
+ userRandom[choix-1].sound[3].pause();
+ userRandom[choix-1].sound[3].currentTime=0;
+ }else if (randomIndex==1) {
+    userRandom[choix-1].sound[0].pause();
+    userRandom[choix-1].sound[0].currentTime=0;
+     userRandom[choix-1].sound[1].play();
+     userRandom[choix-1].sound[1].currentTime=0;
+     userRandom[choix-1].sound[2].pause();
+     userRandom[choix-1].sound[2].currentTime=0;
+     userRandom[choix-1].sound[3].pause();
+     userRandom[choix-1].sound[3].currentTime=0;
+ }else if (randomIndex==2){
+     userRandom[choix-1].sound[0].pause();
+     userRandom[choix-1].sound[0].currentTime=0;
+ userRandom[choix-1].sound[1].pause();
+ userRandom[choix-1].sound[1].currentTime=0;
+ userRandom[choix-1].sound[2].play();
+ userRandom[choix-1].sound[2].currentTime=0;
+ userRandom[choix-1].sound[3].pause();
+ userRandom[choix-1].sound[3].currentTime=0;
+ }else if (randomIndex==3){
+     userRandom[choix-1].sound[0].pause();
+     userRandom[choix-1].sound[0].currentTime=0;
+ userRandom[choix-1].sound[1].pause();
+ userRandom[choix-1].sound[1].currentTime=0;
+ userRandom[choix-1].sound[2].pause();
+ userRandom[choix-1].sound[2].currentTime=0;
+ userRandom[choix-1].sound[3].play();
+ userRandom[choix-1].sound[3].currentTime=0;
+
+ }
+
+
+    })
+
+ for (let b of boxbis){
+        u.addEventListener('mouseover',()=>{
+         
+             choix = u.getAttribute('choice');
+          choicebis=  b.getAttribute('choicebis');
+          
+         if ((choicebis==choix)){
+            b.innerHTML='';
+            let newImageMvt = document.createElement('div');
+            newImageMvt.innerHTML = ` <img src="assets/img/${userRandom[choicebis - 1].imgbis}" alt="${userRandom[choicebis - 1].name}"/>`;
+            b.appendChild(newImageMvt);
+        
+         }})
+
+         u.addEventListener('mouseout',()=>{   
+            choix = u.getAttribute('choice');
+            choicebis=  b.getAttribute('choicebis');
+ 
+
+            if ((choicebis==choix)){
+                b.innerHTML='';
+                let ImageReturn = document.createElement('div');
+                ImageReturn.innerHTML = ` <img src="assets/img/${userRandom[choicebis - 1].img}" alt="${userRandom[choicebis - 1].name}"/>`;
+                b.appendChild(ImageReturn);
+               
+            }})
+        
+        }
+
+    }
+     
 btnClick.addEventListener('click', () => {
-    let numberRandom = Math.ceil(Math.random() * 3);
+    var numberRandom = Math.ceil(Math.random() * 3); 
+    for (us of userRandom[2].sound){
+        us.pause();
+    }
+    for (us of userRandom[0].sound){
+        us.pause();
+    }
+    for (us of userRandom[1].sound){
+        us.pause();
+    }
     jouerSon();
 
-    if  (numberOfRound <12){ 
-        numberOfRound++;
+    if  (numberOfRound <12){
+         
+         numberOfRound++;
         round.innerText= numberOfRound; 
-    
-    // numberOfRound++;
-    
     
     if ((choix == 1) && (numberRandom == 2)) {
         result.innerText = "KO";
@@ -148,36 +310,99 @@ computerAnswer.innerHTML = ''
      victoryUser.innerText=numberOfUserVictory;
    }
 
-   
+//    else if (numberOfRound==12){
+//         console.log("blav")
+//       delete  userRandom[0].sound;
+//       delete userRandom[1].sound;
+//       delete userRandom[2].sound;
+//       } 
    
    }
+   
+   
    if ((numberOfUserVictory>numberOfComputerVictory )&& (numberOfRound==12)){
+   
     jouerSonVictory ();
     soundBell.pause();
-    // bigContainer.style.backgroundImage="url('assets/img/imagevictory.jpg')";
-    result.innerHTML='';
-    let newImageVictory = document.createElement("div");
+    // result.innerHTML='';
+    // let newImageVictory = document.createElement("div");
     
-    newImageVictory.innerHTML=`<div><img src="assets/img/imagevictory.jpg" alt="image victoire"<p>Match gagné!</p></div> ` ;
-    result.appendChild(newImageVictory);
+    // newImageVictory.innerHTML=`<div><img src="assets/img/imagevictory.jpg" alt="image victoire"<p>Match gagné!</p></div> ` ;
+    // result.appendChild(newImageVictory);
+            
+    modal.style.visibility = "visible";
+    modalImage.style.height="100vh";
+    modalImage.style.backgroundImage="url(assets/img/imagevictory.jpg)";
+   modalbis.style.top="0px";
+   sentence.style.transform="translateX(2000%)";
+   sentence.style.left="-100%"; 
+ 
+     
    } else if((numberOfUserVictory<numberOfComputerVictory )&& (numberOfRound==12)) {
+    
     playSoundOfDefeat(); 
     soundBell.pause();
-     result.innerHTML='';
-    let newImageDefaite = document.createElement("div");
     
-     newImageDefaite.innerHTML=`<div><img src="assets/img/defaite.jpg" alt="image victoire"<p>Match perdu!</p></div> ` ;
-     result.appendChild(newImageDefaite);
-
-   } else if ((numberOfUserVictory==numberOfComputerVictory )&& (numberOfRound==12)){
+    //  result.innerHTML='';
+    // let newImageDefaite = document.createElement("div");
+    
+    //  newImageDefaite.innerHTML=`<div><img src="assets/img/defaite.jpg" alt="image victoire"<p>Match perdu!</p></div> ` ;
+    //  result.appendChild(newImageDefaite);
+     modal.style.visibility = "visible";
+     modalImage.style.backgroundImage="url(assets/img/defeat.jpg)";
+     modalImage.style.backgroundPosition="bottom";
+     modalImage.style.height="100vh";
+    modalbis.style.top="0px";
+    sentence.innerText='You have lost';
+    sentence.style.left="-60%";
+    sentence.style.transform="translateX(2000%)"
+ 
+    ; 
+       
+     }
+    else if ((numberOfUserVictory==numberOfComputerVictory )&& (numberOfRound==12)){
+   
     playSoundOfTraining();
      soundBell.pause();
-    
-     result.innerHTML='';
-     let newImageTraining = document.createElement("div");
-     newImageTraining.innerHTML=`<div><img src="assets/gif/rockyTraining.gif" alt="image victoire"<p>You Must Train !</p></div> ` ;
-     result.appendChild(newImageTraining);
-}
+     
+    //  result.innerHTML='';
+    //  let newImageTraining = document.createElement("div");
+    //  newImageTraining.innerHTML=`<div><img src="assets/gif/rockyTraining.gif" alt="image victoire"<p>You Must Train !</p></div> ` ;
+    //  result.appendChild(newImageTraining);
+     modal.style.visibility = "visible";
+     modalImage.style.backgroundImage="url(assets/img/imagevictory.jpg)";
+     modalImage.style.height="100vh";
+    modalbis.style.top="0px";
+    sentence.style.left="-150%";
+    sentence.innerText='You still have to train';
+    sentence.style.transform="translateX(2000%)";
+    // document.body.style.backgroundImage = "url('img_tree.png')"; 
+  
 }
 
-)
+})
+
+buttontoDoAgain.addEventListener('click',()=>{
+    victoryComputer.innerText=0;
+    victoryUser.innerText=0;
+    round.innerText=0;
+    result.innerText='';
+    computerAnswer.innerHTML = '';
+    answerUser.innerHTML='';
+    numberOfRound=0;
+    numberOfUserVictory=0;
+    numberOfComputerVictory=0;
+    modalImage.style.height="0vh";
+
+    modal.style.visibility="hidden";
+  
+   
+   
+   }) 
+
+  
+
+
+
+// for (let e of tableau){
+   
